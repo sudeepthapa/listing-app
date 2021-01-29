83a4cc151a6da2a92eb0319a9ddd84ba3a498cc0
Sudip Thapa <sudeptech@gmail.com>
2021-01-29 18:18:17 +0545

    routing setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import './App.css';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from './components/Navbar';
+import Posts from './pages/Posts';
+import Users from './pages/Users';
 
 function App() {
   return (
     <Router>
       <Navbar />
+      <Switch>
+        <Route exact path="/posts" component={Posts} />
+        <Route exact path="/users" component={Users} />
+      </Switch>
     </Router>
   );
 }
