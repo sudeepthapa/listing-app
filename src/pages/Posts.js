39409e45cf9a4cc898c3f6d089f31b8bf96f932f
Sudip Thapa <sudeptech@gmail.com>
2021-02-01 18:12:48 +0545

    react hooks implemented on posts page

--- src/pages/Posts.js
+++ src/pages/Posts.js
@@ -1,72 +1,114 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import axios from 'axios';
 import {
   Card, Spinner, CardText, CardBody,
-  CardTitle, CardSubtitle, Button, CardFooter
+  CardTitle, CardSubtitle, CardFooter
 } from 'reactstrap';
 
 const BASE_URL = "https://jsonplaceholder.typicode.com"
-class Posts extends React.Component{
+// class Posts extends React.Component{
 
-  state = {
-    posts: [],
-    isLoading:false
-  }
+//   state = {
+//     posts: [],
+//     isLoading:false
+//   }
 
-  componentDidMount = () => {
-    this.setState({
-      ...this.state,
-      isLoading: true
-    })
+//   componentDidMount = () => {
+//     this.setState({
+//       ...this.state,
+//       isLoading: true
+//     })
 
-    axios.get(`${BASE_URL}/posts`).then(res => {
-      this.setState({
-        posts: res.data,
-        isLoading: false
-      })
-    }).catch(error => {
-      this.setState({
-        ...this.state,
-        isLoading: false
-      })
-    })
+//     axios.get(`${BASE_URL}/posts`).then(res => {
+//       this.setState({
+//         posts: res.data,
+//         isLoading: false
+//       })
+//     }).catch(error => {
+//       this.setState({
+//         ...this.state,
+//         isLoading: false
+//       })
+//     })
 
-    // fetch(`${BASE_URL}/posts`).then(res => res.json()).then(json => {
-    //   this.setState({
-    //     posts: json,
-    //     isLoading:false
-    //   })
-    // }).catch(error => {
-    //   this.setState({
-    //     ...this.state,
-    //     isLoading:false
-    //   })
-    //   console.log(error)
-    // })
+//     // fetch(`${BASE_URL}/posts`).then(res => res.json()).then(json => {
+//     //   this.setState({
+//     //     posts: json,
+//     //     isLoading:false
+//     //   })
+//     // }).catch(error => {
+//     //   this.setState({
+//     //     ...this.state,
+//     //     isLoading:false
+//     //   })
+//     //   console.log(error)
+//     // })
     
-  }
+//   }
 
-  render() {
+//   render() {
 
-    if(this.state.isLoading) return <Spinner type="grow" color="primary" />
-    return <div className="container">
-      {
-        this.state.posts.map(post => {
-          return (
-            <Card key={post.id} className="mt-3">
-              <CardBody>
-                <CardTitle tag="h5"> { post.title } </CardTitle>
-                <CardText> { post.body } </CardText>
-              </CardBody>
-              <CardFooter>
-                <CardSubtitle> Created By : {post.userId} </CardSubtitle>
-              </CardFooter>
-            </Card>
-          )
-        })
-      }
-    </div>
-  }
+//     if(this.state.isLoading) return <Spinner type="grow" color="primary" />
+//     return <div className="container">
+//       {
+//         this.state.posts.map(post => {
+//           return (
+//             <Card key={post.id} className="mt-3">
+//               <CardBody>
+//                 <CardTitle tag="h5"> { post.title } </CardTitle>
+//                 <CardText> { post.body } </CardText>
+//               </CardBody>
+//               <CardFooter>
+//                 <CardSubtitle> Created By : {post.userId} </CardSubtitle>
+//               </CardFooter>
+//             </Card>
+//           )
+//         })
+//       }
+//     </div>
+//   }
+// }
+
+
+const Posts = (props) => {
+
+  const [posts, setPosts] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    // Component did mount
+    setIsLoading(true)
+    axios.get(`${BASE_URL}/posts`).then(res => {
+      setPosts(res.data)
+      setIsLoading(false)
+    }).catch(error => {
+      setIsLoading(false)
+    })
+    // Component will unmount
+    return () => {
+      
+    }
+  },[])
+
+  if(isLoading) return <Spinner type="grow" color="primary" />
+  return <div className="container">
+    {
+      posts.map(post => {
+        return (
+          <Card key={post.id} className="mt-3">
+            <CardBody>
+              <CardTitle tag="h5"> { post.title } </CardTitle>
+              <CardText> { post.body } </CardText>
+            </CardBody>
+            <CardFooter>
+              <CardSubtitle> Created By : {post.userId} </CardSubtitle>
+            </CardFooter>
+          </Card>
+        )
+      })
+    }
+  </div>
+  
 }
 
 export default Posts;
